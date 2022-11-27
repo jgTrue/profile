fdc2472104be931b6c5fb9a22c075e8d53ff94b3
Jairo <jgtrue@protonmail.com>
2022-11-27 22:35:52 +0100

    update

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 function displayMenu(id){
     id = document.getElementById(id);
 
