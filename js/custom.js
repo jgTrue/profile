0f0353150801331468183cbb53790db71553ee29
Jairo <jgtrue@protonmail.com>
2022-11-28 15:59:02 +0100

    animation + about me + bg + fix err

--- js/custom.js
+++ js/custom.js
@@ -8,9 +8,25 @@ function displayMenu(id){
     
 }
 
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
+const exampleModal = document.getElementById('contactModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+  const button = event.relatedTarget
+  const recipient = button.getAttribute('data-bs-whatever')
+  const modalTitle = exampleModal.querySelector('.modal-title')
+  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+  modalBodyInput.value = recipient
 })
+
+// Efecto aparecer contenido
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
